6b5864fa261676a4668d0edc3460ff9cc3be238c
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-07-08 18:12:24 -0400

    Updating 07/08/2020 work - almost completed Javascript program

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacterArray =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+
+
 document.getElementById("generate").onclick = function () {
   inputCollection();
 };
 
 function inputCollection() {
-  var PasswordLength = prompt("What is the length of the password");
-  if(!PasswordLength) {
+  var passwordLength = prompt("What is the length of the password");
+  if(!passwordLength) {
     return;
   }
 
-  PasswordLength = Number(PasswordLength);
-  if (PasswordLength < 8 || PasswordLength > 128) {
+  passwordLength = Number(passwordLength);
+  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
     window.alert("INVALID PASSWORD LENGTH \nPlease Enter a number between 8 and 128");
     inputCollection();
   } else {
@@ -23,11 +31,51 @@ function inputCollection() {
     console.log(inputSpecial);
     var inputNumber = window.confirm("Do you want to include NUMBERS ?");
     console.log(inputNumber);
+    generatePassword(passwordLength,inputCapital,inputSmall,inputSpecial,inputNumber);
 
   }
+
   if((inputCapital==false) && (inputSmall ==false) && (inputSpecial==false) && (inputNumber ==false)) {
     window.alert("Password Must contain UPPERCASE letter or LOWERCASE letter or SPECIAL character or NUMBERS");
     inputCollection();
   }
 }
 
+function generatePassword(passwordLength,inputCapital,inputSmall,inputSpecial,inputNumber) {
+  var newPassword = "";
+  var i=0;
+  var random;
+  console.log((passwordLength));
+  console.log(inputCapital);
+  console.log(inputSmall);
+  console.log(inputSpecial);
+  console.log(inputNumber);
+     do {
+        if((inputCapital==true) && (i<passwordLength)) {
+          random = Math.floor(Math.random() * uppercaseArray.length); 
+          console.log(random);
+          newPassword += uppercaseArray[random];
+          i++
+        }
+        if((inputSmall==true) && (i<passwordLength)) {
+          random = Math.floor(Math.random() * lowercaseArray.length); 
+          newPassword += lowercaseArray[random];
+          i++
+        }
+
+        if((inputSpecial==true) && (i<passwordLength))  {
+          random = Math.floor(Math.random() * specialCharacterArray.length); 
+          newPassword += specialCharacterArray[random];
+          i++
+        }
+        if((inputNumber==true) && (i<passwordLength))  {
+          random = Math.floor(Math.random() * numbersArray.length); 
+          newPassword += numbersArray[random];
+          i++
+        }
+           
+      } while(i<passwordLength);
+      console.log('The newly generated password is: ' + newPassword + ' and LENGTH is: ' + newPassword.length);
+      window.alert(newPassword);
+      
+    }
